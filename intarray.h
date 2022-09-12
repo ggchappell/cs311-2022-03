63e0594130687c265da2ad16ebf1993868c842e3
Glenn G. Chappell <ggchappell@alaska.edu>
2022-09-12 11:31:32 -0800

    Add intarray.h (UNFINISHED)

---
+++ intarray.h
@@ -0,0 +1,44 @@
+// intarray.h  UNFINISHED
+// Glenn G. Chappell
+// 2022-09-12
+//
+// For CS 311 Fall 2022
+// Header for class IntArray
+// Simple RAII class holding dynamic array of int.
+// There is no associated source file.
+
+#ifndef FILE_INTARRAY_H_INCLUDED
+#define FILE_INTARRAY_H_INCLUDED
+
+
+// *********************************************************************
+// class IntArray - Class definition
+// *********************************************************************
+
+
+// class IntArray
+// Simple RAII class holding dynamic array of int.
+// A const IntArray does not allow modification of its array items.
+class IntArray {
+
+// ***** IntArray: Types *****
+public:
+
+
+// ***** IntArray: Ctors, dctor, op= *****
+public:
+
+
+// ***** IntArray: General public operators *****
+public:
+
+
+// ***** IntArray: Data members *****
+private:
+
+
+};  // End class IntArray
+
+
+#endif  //#ifndef FILE_INTARRAY_H_INCLUDED
+
