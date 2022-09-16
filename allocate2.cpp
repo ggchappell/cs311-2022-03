7df810cd58d376537b511c2452c3708b420d5296
Glenn G. Chappell <ggchappell@alaska.edu>
2022-09-16 14:22:46 -0800

    allocate2.cpp: FINISH - write func allocate2

--- allocate2.cpp
+++ allocate2.cpp
@@ -1,4 +1,4 @@
-// allocate2.cpp  UNFINISHED
+// allocate2.cpp
 // Glenn G. Chappell
 // 2022-09-16
 //
@@ -97,9 +97,21 @@ private:
 void allocate2(Named * & nptra,
                Named * & nptrb)
 {
-    // TODO: This function isn't right; finish it!
     nptra = new Named("Object A");
-    nptrb = new Named("Object B");
+    // If the 1st allocation fails, then the "new" throws, and we leave.
+    // No clean-up necessary.
+
+    try
+    {
+        nptrb = new Named("Object B");
+        // If the 2nd allocation fails, then we need to clean up the
+        // first.
+    }
+    catch (...)
+    {
+        delete nptra;
+        throw;  // We cannot handle the exception here; re-throw it
+    }
 }
 
 
