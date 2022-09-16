4a4c6a73b8eac40552feb5fe9a40d17d88a692cb
Glenn G. Chappell <ggchappell@alaska.edu>
2022-09-15 17:50:07 -0800

    Add range.py

---
+++ range.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# range.py
+# Glenn G. Chappell
+# 2022-09-15
+#
+# For CS 311 Fall 2022
+"""Example of a range specification in Python.
+Requires Python 3."""
+
+
+import sys  # for .exit
+
+
+# main
+def main():
+    """Demonstrate Python's range functionality.
+
+    """
+    print("Printing values given by \"range(2, 7)\":")
+    for i in range(2, 7):
+        print(f"  {i}")
+    print()
+
+    print("Python follows the same first/just-past-last convention for")
+    print("specifying ranges as the C++ STL.")
+    print()
+
+    return 0  # Success
+
+
+# If this module is executed as a program (instead of being imported):
+# Call function main and exit with its return value.
+if __name__ == "__main__":
+    sys.exit(main())
+
