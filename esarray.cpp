cba96ee3980531409cdbcea064d9d5c059f337e7
Glenn G. Chappell <ggchappell@alaska.edu>
2022-10-18 15:49:19 -0800

    Add esarray.h/.cpp (UNFINISHED)

---
+++ esarray.cpp
@@ -0,0 +1,18 @@
+// esarray.cpp  UNFINISHED
+// Glenn G. Chappell
+// 2022-10-18
+//
+// For CS 311 Fall 2022
+// Source for class ESArray
+// Extremely smart array of int
+// Preliminary to Assignment 5
+
+
+#include "esarray.h"  // For class ESArray definition
+
+
+// *********************************************************************
+// class ESArray - Member function definitions
+// *********************************************************************
+
+
---
+++ esarray.h
@@ -0,0 +1,50 @@
+// esarray.h  UNFINISHED
+// Glenn G. Chappell
+// 2022-10-18
+//
+// For CS 311 Fall 2022
+// Header for class ESArray
+// Extremely smart array of int
+// Preliminary to Assignment 5
+
+#ifndef FILE_ESARRAY_H_INCLUDED
+#define FILE_ESARRAY_H_INCLUDED
+
+
+// *********************************************************************
+// class ESArray - Class definition
+// *********************************************************************
+
+
+// class ESArray
+// Extremely Smart Array of int.
+// Resizable, copyable/movable, exception-safe.
+// Invariants:
+//     ???
+class ESArray {
+
+// ***** ESArray: types *****
+public:
+
+
+// ***** ESArray: ctors, op=, dctor *****
+public:
+
+
+// ***** ESArray: general public operators *****
+public:
+
+
+// ***** ESArray: general public functions *****
+public:
+
+
+// ***** ESArray: data members *****
+private:
+
+
+};  // End class ESArray
+
+
+#endif  //#ifndef FILE_ESARRAY_H_INCLUDED
+
