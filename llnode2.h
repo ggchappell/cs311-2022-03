d1e368a53d3027b979fdff2cd448846c60e21f3c
Glenn G. Chappell <ggchappell@alaska.edu>
2022-10-28 11:56:19 -0800

    Add llnode2.h (UNFINISHED)

---
+++ llnode2.h
@@ -0,0 +1,91 @@
+// llnode2.h  UNFINISHED
+// Glenn G. Chappell
+// 2022-10-28
+//
+// For CS 311 Fall 2022
+// Header for struct LLNode2
+// Singly Linked List node
+// Based on llnode.h
+// There is no associated source file.
+
+#ifndef FILE_LLNODE2_H_INCLUDED
+#define FILE_LLNODE2_H_INCLUDED
+
+
+// *********************************************************************
+// struct LLNode2 - Struct definition
+// *********************************************************************
+
+
+// struct LLNode2
+// Singly Linked List node, with client-specified value type
+// Invariants:
+//     Either _next is nullptr, or _next points to an LLNode2, allocated
+//      with new, owned by *this.
+// Requirements on Types:
+//     ValType must have a copy ctor and a (non-throwing) dctor.
+template <typename ValType>
+struct LLNode2 {
+    ValType   _data;  // Data for this node
+    LLNode2 *  _next;  // Ptr to next node, or nullptr if none
+
+    // The following simplify creation & destruction
+
+    // 1- & 2-param ctor
+    // _data is set to data (given). _next is set to next, if given, or
+    // nullptr if not.
+    // Pre:
+    //     theNext, if passed, is either nullptr, or else it points to
+    //      an LLNode2 allocated with new, with ownership transferred to
+    //      *this.
+    explicit LLNode2(const ValType & data,
+                     LLNode2 * next = nullptr)
+        :_data(data),
+         _next(next)
+    {}
+
+    // dctor
+    // Does delete on _next.
+    ~LLNode2()
+    { delete _next; }
+
+    // No default ctor, no copy/move operations.
+    LLNode2() = delete;
+    LLNode2(const LLNode2 & other) = delete;
+    LLNode2 & operator=(const LLNode2 & other) = delete;
+    LLNode2(LLNode2 && other) = delete;
+    LLNode2 & operator=(LLNode2 && other) = delete;
+
+};  // End struct LLNode2
+
+
+// *********************************************************************
+// struct LLNode2 - Associated global functions
+// *********************************************************************
+
+
+// size
+// Given ptr to Linked List, return its size (number of nodes).
+// Pre:
+//     head is ptr to nullptr-terminated Linked List, or nullptr for
+//      empty Linked List.
+// Requirements on Types:
+//     ValType must have a copy ctor and a (non-throwing) dctor.
+// NOTE: The above are the requirements for LLNode2<ValType>; no member
+// functions of ValType are actually used here.
+template <typename ValType>
+size_t size(const LLNode2<ValType> * head)
+{
+    auto p = head;       // Iterates through list
+    size_t counter = 0;  // Number of nodes so far
+    while (p != nullptr)
+    {
+        p = p->_next;
+        ++counter;
+    }
+    return counter;
+}
+
+
+#endif  //#ifndef FILE_LLNODE2_H_INCLUDED
+
